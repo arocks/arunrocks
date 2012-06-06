aea9f4f8fb7a98b163dc8e6cc8ecdc1dcd7b1fba
Arun Ravindran <arunvr@gmail.com>
2012-06-07 00:36:55 +0530

    Changed all modernizer loaded scripts to async. Removed addthis

--- javascripts/site.js
+++ javascripts/site.js
@@ -12,4 +12,16 @@ $(document).ready(function () {
     $(document).bind('keydown', 'alt+b',      function() {go_page('previous');});
     $(document).bind('keydown', 'ctrl+right',     function() {go_page('next');});
     $(document).bind('keydown', 'alt+f',     function() {go_page('next');});
+    
+    // Add social buttons
+    // 1. Hacker News
+    (function(d, t) {
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.src = '//hnbutton.appspot.com/static/hn.js';
+        s.parentNode.insertBefore(g, s);
+    }(document, 'script'));
+
+    $("#socialbutns").append(
+        '<a href="http://news.ycombinator.com/submit" class="hn-share-button">Vote on HN</a>');
 });
